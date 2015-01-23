1dcfe85b5e414db59837cd2d4f18d5ba3cf6b681
Robson Duarte <robson.o.d@gmail.com>
2015-01-22 22:42:46 -0200

    new test for lazy operations streams.

--- stream-api/src/test/java/br/com/javamagazine/stream/api/Java8Test.java
+++ stream-api/src/test/java/br/com/javamagazine/stream/api/Java8Test.java
@@ -1,36 +1,58 @@
 package br.com.javamagazine.stream.api;
 
+import static br.com.javamagazine.stream.api.Java8.getAsCincoPrimeirasPessoasMaiorDeIdade;
 import static br.com.javamagazine.stream.api.Java8.getNumeroDePessoasMaiorDeIdade;
 import static java.util.Arrays.asList;
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+import static org.junit.Assert.assertThat;
 
 import java.util.List;
 
 import org.junit.Test;
 
-import br.com.javamagazine.stream.api.Pessoa;
-
 public class Java8Test {
 
-	
+
 	@Test
 	public void test() {
-		assertEquals(9, getNumeroDePessoasMaiorDeIdade(getPessoas()));;
+		long numeroDePessoasMaiorDeIdade = getNumeroDePessoasMaiorDeIdade(getPessoas());
+		assertThat(numeroDePessoasMaiorDeIdade, equalTo(9l));
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+	@Test
+	public void test2() {
+
+		List<Pessoa> pessoas = getAsCincoPrimeirasPessoasMaiorDeIdade(getPessoas());
+
+		assertThat(pessoas,hasItems(
+						new Pessoa(18, 'M'),
+						new Pessoa(20, 'M'),
+						new Pessoa(22, 'M'),
+						new Pessoa(50, 'M'),
+						new Pessoa(88, 'M')
+					)
+			);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	// TODO: criar uma class helper ??
 	private List<Pessoa> getPessoas() {
 		return asList(

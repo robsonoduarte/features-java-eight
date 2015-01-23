d2accaf97d81c25e48a511bea067bbb9ea498f2e
Robson Duarte <robson.o.d@gmail.com>
2015-01-22 22:41:07 -0200

    add hascode, equals, toString methods for use in assert unit tests.

--- stream-api/src/main/java/br/com/javamagazine/stream/api/Pessoa.java
+++ stream-api/src/main/java/br/com/javamagazine/stream/api/Pessoa.java
@@ -1,12 +1,67 @@
 package br.com.javamagazine.stream.api;
 
+import static org.apache.commons.lang3.builder.EqualsBuilder.reflectionEquals;
+import static org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode;
+import static org.apache.commons.lang3.builder.ToStringBuilder.reflectionToString;
+import static org.apache.commons.lang3.builder.ToStringStyle.SHORT_PREFIX_STYLE;
+
 public class Pessoa {
 
-	
+
 	public int idade;
 
-	
+
 	public Pessoa(int idade, char sexo) {
 		this.idade = idade;
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	@Override
+	public int hashCode() {
+		return reflectionHashCode(this);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		return reflectionEquals(this, obj);
+	}
+
+
+
+	@Override
+	public String toString() {
+		return reflectionToString(this, SHORT_PREFIX_STYLE);
+	}
+
 }

2e8e3c15da730c8eb7da6fc22266952b661a7027
Robson Duarte <robson.o.d@gmail.com>
2015-01-22 22:42:10 -0200

    new method for test de lazy operations.

--- stream-api/src/main/java/br/com/javamagazine/stream/api/Java8.java
+++ stream-api/src/main/java/br/com/javamagazine/stream/api/Java8.java
@@ -1,14 +1,26 @@
 package br.com.javamagazine.stream.api;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.List;
 
 public class Java8 {
 
-	public static long getNumeroDePessoasMaiorDeIdade(List<Pessoa> pessoas) {
-		
+	public static long getNumeroDePessoasMaiorDeIdade(List<Pessoa> pessoas){
 		return pessoas.stream()
 				.filter(p -> p.idade >= 18)
 				.count();
 	}
+
+
+	public static List<Pessoa> getAsCincoPrimeirasPessoasMaiorDeIdade(List<Pessoa> pessoas){
+		return pessoas.stream()
+				.filter(p -> p.idade >= 18)
+				.peek(p -> System.out.println(p))
+				.limit(5)
+				.collect(toList());
+
+	}
+
 
 }

cec9703b61c4bc4bdecbaf32521bd82ef067f012
robsonoduarte <robson.o.d@gmail.com>
2015-02-12 00:22:49 -0200

    starting examples about reduction operations.

---
+++ stream-api/src/test/java/br/com/javamagazine/stream/api/SimpleReductions.java
@@ -0,0 +1,40 @@
+package br.com.javamagazine.stream.api;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Test;
+
+public class SimpleReductions extends BasicTest{
+
+
+	@Test
+	public void test() {
+		
+		List<Pessoa> pessoas = getPessoas();
+		
+		Optional<Pessoa> pessoa = 
+				pessoas.stream().max(Comparator.comparing(p -> p.idade));
+
+		assertThat(pessoa.get().idade, equalTo(88));
+		
+	}
+	
+	
+	@Test
+	public void test1() {
+		
+		List<Pessoa> pessoas = getPessoas();
+		
+		Optional<Pessoa> pessoa = 
+				pessoas.stream().min(Comparator.comparing(p -> p.idade));
+		
+		assertThat(pessoa.get().idade, equalTo(1));	
+	}
+
+	
+}
---
+++ stream-api/src/test/java/br/com/javamagazine/stream/api/StreamStatefulTransformationTest.java
@@ -0,0 +1,15 @@
+package br.com.javamagazine.stream.api;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class StreamStatefulTransformationTest {
+
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

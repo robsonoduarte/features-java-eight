3dc179873b69ace23b5c0f1d6ba4e8c4854a8e81
robsonoduarte <robson.o.d@gmail.com>
2015-02-12 00:16:53 -0200

    add example abou skip method.

--- stream-api/src/test/java/br/com/javamagazine/stream/api/StreamExtractingAndCombining.java
+++ stream-api/src/test/java/br/com/javamagazine/stream/api/StreamExtractingAndCombining.java
@@ -5,12 +5,16 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.Test;
 
-public class StreamExtractingAndCombining {
+public class StreamExtractingAndCombining extends BasicTest{
 
 	@Test
 	public void test() {						
@@ -23,15 +27,16 @@ public class StreamExtractingAndCombining {
 	}
 
 	
-	
 	@Test
-	public void test2() {
+	public void test2() throws IOException {
 		
-		List<Double> list = 
-				Stream.generate(Math::random).limit(10).collect(toList());
 		
-		assertThat(list, hasSize(10));
+		Path path = Paths.get("src/test/resources/file.txt");
 		
+		List<String> list = 
+				Files.lines(path).skip(2).collect(toList());
+		
+		assertThat(list, hasSize(8));
 	}
 	
 	
@@ -39,7 +44,10 @@ public class StreamExtractingAndCombining {
 	
 	@Test
 	public void test3() {
-				
+		
+		getPessoas().stream().filter(p -> p.idade < 10 || p.idade > 20).forEach(System.out::println);
+		
+		/*Stream.concat(a, b)*/
 	}
 	
 

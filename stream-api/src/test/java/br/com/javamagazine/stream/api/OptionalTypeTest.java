7cde1ecbece1af349b242814b98494590de2a7f4
robsonoduarte <robson.o.d@gmail.com>
2015-03-03 18:54:39 -0300

    starting the examples about OptionalTypes.

---
+++ stream-api/src/test/java/br/com/javamagazine/stream/api/OptionalTypeTest.java
@@ -0,0 +1,14 @@
+package br.com.javamagazine.stream.api;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class OptionalTypeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

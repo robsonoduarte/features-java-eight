aacad7ff7ba84777e3292af1fcd60ae58858a6c7
robsonoduarte <robson.o.d@gmail.com>
2015-03-04 23:43:37 -0300

    developing de Optional<T> examples...

--- stream-api/src/test/java/br/com/javamagazine/stream/api/OptionalTypeTest.java
+++ stream-api/src/test/java/br/com/javamagazine/stream/api/OptionalTypeTest.java
@@ -1,14 +1,52 @@
 package br.com.javamagazine.stream.api;
 
-import static org.junit.Assert.fail;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.junit.Test;
 
-public class OptionalTypeTest {
+public class OptionalTypeTest extends BasicTest{
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	
+	@Test(expected=NoSuchElementException.class)
+	public void test1() {
+		
+		Optional<Pessoa> primeiraPessoa = 
+				pessoas.stream().filter(p -> p.nome.startsWith("W")).findFirst();
+	
+		primeiraPessoa.get();
+	}
+	
+
+	
+	
+	public void test2() {
+				
+		Optional<Pessoa> primeiraPessoa = 
+				pessoas.stream().filter(p -> p.nome.startsWith("A")).findFirst();
+		
+		assertThat(primeiraPessoa.isPresent(), equalTo(true));
+	}
+	
+	
+	
+	public void test3() {
+	
+		Pessoa pessoa = new Pessoa(1, "x");
+		
+		if(pessoa != null){
+			// alguma operação com o objeto
+		}
+
+		
+		
 	}
 
+	
+	
+	
 }

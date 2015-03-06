604930ff04b58db4d417b9583e1925149c3fa5e5
robsonoduarte <robson.o.d@gmail.com>
2015-03-06 00:10:38 -0300

    return de ArrayList

--- stream-api/src/test/java/br/com/javamagazine/stream/api/BasicTest.java
+++ stream-api/src/test/java/br/com/javamagazine/stream/api/BasicTest.java
@@ -2,6 +2,7 @@ package br.com.javamagazine.stream.api;
 
 import static java.util.Arrays.asList;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
@@ -29,23 +30,23 @@ public class BasicTest {
 
 	
 	private List<Pessoa> getPessoas() {
-		return asList(
-				new Pessoa(18,"Pedro"),
-	 			new Pessoa(10,"Maria"),
-	 			new Pessoa(11,"Joao"),
-	 			new Pessoa(20,"Antonio"),
-	 			new Pessoa(22,"Ana"),
-	 			new Pessoa(50,"Guilherme"),
-	 			new Pessoa(12,"Gustavo"),
-	 			new Pessoa(88,"Alexandre"),
-	 			new Pessoa(26,"Peterson"),
-	 			new Pessoa(38,"Daniel"),
-	 			new Pessoa(29,"Giovana"),
-	 			new Pessoa(39,"Sidney"),
-	 			new Pessoa(9,"Everaldo"),
-	 			new Pessoa(1,"Julio"),
-	 			new Pessoa(3,"Marco")
-			);
+		return new ArrayList<>(asList(
+						new Pessoa(18,"Pedro"),
+			 			new Pessoa(10,"Maria"),
+			 			new Pessoa(11,"Joao"),
+			 			new Pessoa(20,"Antonio"),
+			 			new Pessoa(22,"Ana"),
+			 			new Pessoa(50,"Guilherme"),
+			 			new Pessoa(12,"Gustavo"),
+			 			new Pessoa(88,"Alexandre"),
+			 			new Pessoa(26,"Peterson"),
+			 			new Pessoa(38,"Daniel"),
+			 			new Pessoa(29,"Giovana"),
+			 			new Pessoa(39,"Sidney"),
+			 			new Pessoa(9,"Everaldo"),
+			 			new Pessoa(1,"Julio"),
+			 			new Pessoa(3,"Marco"))
+				);
 	}
 
 }

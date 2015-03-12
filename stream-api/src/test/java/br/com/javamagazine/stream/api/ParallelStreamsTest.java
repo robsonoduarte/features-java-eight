3c93917ea0c0209fb62b834b025f8771fbe994a1
robsonoduarte <robson.o.d@gmail.com>
2015-03-11 23:40:59 -0300

    starting example about Primite Type Streams and Parallel Streams.

---
+++ stream-api/src/test/java/br/com/javamagazine/stream/api/ParallelStreamsTest.java
@@ -0,0 +1,14 @@
+package br.com.javamagazine.stream.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ParallelStreamsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ stream-api/src/test/java/br/com/javamagazine/stream/api/PrimiteTypeStreamsTest.java
@@ -0,0 +1,14 @@
+package br.com.javamagazine.stream.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PrimiteTypeStreamsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

df4bb312c94e66bff3e5ffa57b0d0f40641e235d
Robson <Robson@bravox>
2015-03-19 23:12:00 -0300

    finished test to method flatmap.

--- stream-api/src/test/java/br/com/javamagazine/stream/api/StreamTransformationTest.java
+++ stream-api/src/test/java/br/com/javamagazine/stream/api/StreamTransformationTest.java
@@ -1,11 +1,14 @@
 package br.com.javamagazine.stream.api;
 
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 import static org.apache.commons.lang3.StringUtils.isAllUpperCase;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -25,22 +28,21 @@ public class StreamTransformationTest extends BasicTest {
 	}
 
 	
-	@Test
-	public void testFlatMapMethodForStreamTransformation() {
 	
-		//http://www.adam-bien.com/roller/abien/entry/java_8_flatmap_example
+	@Test
+	public void testFlatMapMethodForStreamTransformation() {		
+		Set<String> hobbys = 
+			pessoas.stream()
+				.filter(p -> p.getIdade() >= 18)
+				.map(p -> p.getHobbys())
+				.flatMap(l -> l.stream())
+				.collect(toSet());
 		
+
+		assertThat(hobbys, hasSize(11));
 		
-		//http://www.journaldev.com/2774/java-8-stream-api-example-tutorial
-		//http://pt.slideshare.net/chris.e.richardson/map-flatmap-and-reduce-are-your-new-best-friends-simpler-collections-concurrency-and-big-data-jax-jax2014
-/*		pessoas.stream()
-			.flatMap(p -> p.nome = p.nome.toUpperCase())
-			.collect(toList());*/
+	
 	}
 
-	
 
-/*	public Stream<String> */
-	
-	
 }

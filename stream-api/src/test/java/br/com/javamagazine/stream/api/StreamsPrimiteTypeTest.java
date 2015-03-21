b86d947aa6f7202dc7edb7ccf6c1e61cfb9afaa2
robsonoduarte <robson.o.d@gmail.com>
2015-03-20 23:31:53 -0300

    coment the method test.

--- stream-api/src/test/java/br/com/javamagazine/stream/api/StreamsPrimiteTypeTest.java
+++ stream-api/src/test/java/br/com/javamagazine/stream/api/StreamsPrimiteTypeTest.java
@@ -1,14 +1,12 @@
 package br.com.javamagazine.stream.api;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class StreamsPrimiteTypeTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 }

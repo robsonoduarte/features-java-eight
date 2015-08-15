e7e2b8e2f83d5c8c59368c08d112cb235bac632a
Robson Duarte <robson.o.d@gmail.com>
2015-08-15 11:23:04 -0300

    add example of how use lambda..

--- lambda-expressions/src/main/java/br/com/mystudies/lambda/expressions/FilePath.java
+++ lambda-expressions/src/main/java/br/com/mystudies/lambda/expressions/FilePath.java
@@ -1,5 +1,30 @@
 package br.com.mystudies.lambda.expressions;
 
+import java.io.File;
+import java.io.FileFilter;
+
 public class FilePath {
+
+	public static void main(String[] args) {
+
+
+
+		new File("").listFiles(new FileFilter() {
+
+			@Override
+			public boolean accept(File pathname) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+		});
+
+
+
+
+		new File("").listFiles(f -> {return true;});
+
+
+	}
+
 
 }

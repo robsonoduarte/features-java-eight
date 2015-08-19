8add14ae2c3c36f98dbc12c98c764b38019f9e6a
Robson <Robson@java>
2015-08-18 22:50:12 -0300

    another example using lambda.

---
+++ lambda-expressions/src/main/java/br/com/mystudies/lambda/expressions/FilePath2.java
@@ -0,0 +1,44 @@
+package br.com.mystudies.lambda.expressions;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class FilePath2 {
+
+	public static void main(String[] args) {
+
+		String[] files;
+	
+
+		
+		// without lambda
+		
+		files =
+			new File("/teste/").list(new FilenameFilter() {			
+			@Override
+			public boolean accept(File file, String name) {						
+				return name.contains("txt");
+			}
+		});
+
+		
+	
+		/*asList(files).stream().forEach(System.out::println);*/
+		
+
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+
+}

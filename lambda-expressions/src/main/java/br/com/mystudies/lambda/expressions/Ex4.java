f89617d61f6d6c1cd1e12aa49a94e21bad947b0f
Robson Duarte <robson.o.d@gmail.com>
2016-03-01 21:40:11 -0300

    ex 4.

--- lambda-expressions/src/main/java/br/com/mystudies/lambda/expressions/Ex4.java
+++ lambda-expressions/src/main/java/br/com/mystudies/lambda/expressions/Ex4.java
@@ -1,21 +1,76 @@
 package br.com.mystudies.lambda.expressions;
 
-import static java.util.Arrays.asList;
-
 import java.io.File;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
 
 public class Ex4 {
 
 
 
-	public static void main(String[] args) {
 
 
-		asList(new File("/").list())
-			.stream()
-			.sorted()
+	public static void main(String[] args) throws Exception {
+		Files.walk(Paths.get(path()))
+			.map(Path::toFile)
+			.sorted(sort())
 			.forEach(System.out::println);
+	}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	private static Comparator<? super File> sort() {
+		return (f1,f2) -> {
+			   if (f1.isDirectory() && !f2.isDirectory()) {
+	                return -1;
+	            } else if (!f1.isDirectory() && f2.isDirectory()) {
+	                return 1;
+	            } else {
+	                return f1.getName().toLowerCase().compareTo(f2.getName().toLowerCase());
+	            }
+		};
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	private static URI path() throws Exception {
+		return Ex4.class.getResource("/groups").toURI();
 	}
 
 

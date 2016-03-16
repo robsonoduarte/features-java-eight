9a5999967cfffb72e1252e7d1c23801f17c3c429
Robson Duarte <robson.o.d@gmail.com>
2016-03-15 21:42:46 -0300

    finished ex8

--- lambda-expressions/src/main/java/br/com/mystudies/lambda/expressions/Ex8.java
+++ lambda-expressions/src/main/java/br/com/mystudies/lambda/expressions/Ex8.java
@@ -14,12 +14,57 @@ public class Ex8 {
 		List<Runnable> runners = new ArrayList<>();
 
 		asList("Robson", "Luiz", "Wellington")
-			.forEach(s -> {
-				runners.add(() -> print(s));
+			.forEach(name -> {
+				runners.add(() -> print(name));
 			});
 
 
+		run(runners);
 
+
+
+
+
+
+
+
+
+		String[] names = {"Ana Mara", "Cida", "Maria"};
+
+		for (int i = 0; i < names.length; i++) {
+			//runners.add(() -> print(names[i])); this don't compile because in lambda the variables need is immutable and the (i) isn't
+		}
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	private static void run(List<Runnable> runners) {
+		runners.forEach(r -> {
+			new Thread(r).start();
+		});
+		runners.clear();
 	}
 
 
